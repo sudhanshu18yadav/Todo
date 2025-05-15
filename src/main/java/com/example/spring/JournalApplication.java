0a28f4cc2a830dd37376664b99134464a331bb21
yadavsudhanshu1824@gmail.com <yadavsudhanshu1824@gmail.com>
2025-05-15 17:03:18 +0530

    cleaning

--- src/main/java/com/example/spring/JournalApplication.java
+++ src/main/java/com/example/spring/JournalApplication.java
@@ -6,16 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication 
 public class JournalApplication {
 
-	// @Bean
-	// InitializingBean saveData(JournalRepository repo) {
-	// 	return ()->{
-	// 		repo.save(new Journal("get to know spring boots", "todfay i will learn spring boopts", "01/01/2016"));
-	// 		repo.save(new Journal("spring boots reading reading", "reading spree", "01/01/2016"));
-	// 		repo.save(new Journal("spring boots cloud", "the cloud", "01/01/2016"));
-	// 		repo.save(new Journal("spring boots backend dev", "the final backend", "01/01/2016"));
-	// 	};
-	// }
-
 	public static void main(String[] args) {
 		SpringApplication.run(JournalApplication.class, args);
 	}
--- src/main/java/com/example/spring/repository/JournalRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.spring.domain.Journal;
-
-public interface JournalRepository extends JpaRepository<Journal, Long> {
-} 
